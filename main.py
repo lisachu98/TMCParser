64bffdaa302df5c50dff05822d70fc9475d89948
Max <maksyminator@gmail.com>
2023-05-15 16:33:56 +0200

    V2

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import shapefile
 import binascii
 import struct
 
-path='poland.shp'
+path='cropPoints.shp'
 
 with open(path, 'rb') as f:
     # Odczytaj nagłówek pliku shapefile (100 bajtów)
     header = f.read(100)
 
-    # Odczytaj magiczną liczbę (4 bajty)
-    magic_number = int.from_bytes(header[:4], byteorder='big')
-    print('Magiczna liczba:', magic_number)
+    # Odczytaj magiczną liczbę - kod pliku (4 bajty)
+    file_code = int.from_bytes(header[:4], byteorder='big')
+    print('Kod pliku:', file_code)
 
     # Odczytaj wersję (4 bajty)
     version = int.from_bytes(header[28:32], byteorder='little')
@@ -25,31 +25,69 @@ with open(path, 'rb') as f:
     print('Bounding box:', bbox)
 
     # Odczytaj liczbę rekordów (4 bajty)
-    num_records = int.from_bytes(header[24:28], byteorder='big')
-    print('Liczba rekordów:', num_records)
+    file_length = int.from_bytes(header[24:28], byteorder='little')
+    print('Długość pliku (file length):', file_length)
 
     f.seek(24) # przejście do pozycji 24 w pliku
     checksum = binascii.hexlify(f.read(4)).decode("utf-8") # odczytanie 4 bajtów i przekształcenie do postaci heksadecymalnej
     print("Suma kontrolna:", checksum)
 
-    f.seek(24)
-    file_length = int.from_bytes(f.read(4), byteorder='big') * 2
+    # Odczytaj nagłówek rekordów głównego pliku
+    main_header = f.read(100)
 
-    # Odczytaj długość nagłówka SHP (100 bajtów)
-    header_length = 100
+    # Odczytaj numer rekordu (4 bajty)
+    # record_number = int.from_bytes(main_header[0:4], byteorder='big')
+    # print('Numer rekordu:', record_number)
 
-    # Oblicz długość zawartości SHP
-    content_length = file_length - header_length
+    # Odczytaj długość zawartości rekordu (4 bajty)
+    # content_length = int.from_bytes(main_header[4:8], byteorder='big')
+    # print('Długość zawartości rekordu:', content_length)
 
-    # Wyświetl wyniki
-    print("File length:", file_length)
-    print("Content length:", content_length)
 
-    while True:
-        record = f.read(8)
-        if not record:
-            break
-        recHeader = int.from_bytes(record[:4], byteorder='big')
-        recLength = int.from_bytes(record[4:], byteorder='big')
-        print("Record header:", recHeader)
-        print("Record length:", recLength)
+
+    sf = shapefile.Reader(path, encoding="iso-8859-1")
+
+    # odczytanie atrybutów pliku shapefile
+    fields = [x[0] for x in sf.fields][1:]
+    records = sf.records()
+
+    # odczytanie geometrii obiektów pliku shapefile
+    shapes = sf.shapes()
+
+    # liczba obiektów
+    leng = len(shapes)
+
+    print(f"Liczba obiektow: {leng}")
+
+    print("****************************")
+
+    with shapefile.Reader(path, encoding="iso-8859-1") as sf:
+        fields = [x[0] for x in sf.fields][1:]
+        records = sf.records()
+        shapes = sf.shapes()
+        total_content_length=0
+
+        # wyświetlenie atrybutów i geometrii obiektów pliku shapefile
+        for i, rec in enumerate(records):
+            shape = shapes[i]
+            content_length = len(rec)
+            print(f"Atrybuty obiektu {i + 1}: {rec}")
+            print(f"Geometria obiektu {i + 1}: {shape}")
+            print(f"Długość rekordu: {len(rec)}")
+
+            # Obliczenie długości zawartości rekordu
+            content_length = 4  # nagłówek rekordu zajmuje 4 bajty
+            content_length += 8 * len(shape.parts)  # liczba części geometrii * 8 bajtów na część
+            content_length += 16 * len(shape.points)  # liczba punktów geometrii * 16 bajtów na punkt
+
+            # Wyświetlenie długości zawartości rekordu
+            print(f"Długość zawartości rekordu (content length): {content_length}")
+
+            # Dodanie obliczonej długości do sumy długości rekordów
+            total_content_length += content_length
+            print("****************************")
+
+# Wyświetlenie całkowitej długości zawartości rekordów
+print(f"Całkowita długość zawartości rekordów: {total_content_length}")
+
+
---
+++ records.py
@@ -0,0 +1,79 @@
+import shapefile
+import numpy as np
+import matplotlib.pyplot as plt
+from dbfread import DBF
+
+path = 'poland.dbf'  # Ścieżka do pliku DBF
+
+table = DBF(path, encoding='latin-1')
+
+records = list(table)  # Odczytaj wszystkie rekordy
+
+print(len(records))  # Liczba rekordów w pliku
+
+# Get the first shape and record
+sf = shapefile.Reader('poland.shp')
+shape_ex = sf.shape(3)  # Odczytaj czwarty kształt (indeks 3) - woj kuj pom
+
+record = records[3]  # Odczytaj czwarty rekord (indeks 3)
+
+x_lon = np.zeros((len(shape_ex.points), 1))
+y_lat = np.zeros((len(shape_ex.points), 1))
+
+for ip in range(len(shape_ex.points)):
+    x_lon[ip] = shape_ex.points[ip][0]
+    y_lat[ip] = shape_ex.points[ip][1]
+
+# Plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.get_yaxis().set_visible(True)
+ax.get_xaxis().set_visible(True)
+ax.set_frame_on(False)
+plt.plot(x_lon, y_lat, 'k')
+
+# Ustawiane są ograniczenia wykresu używając danych z bounding box (bbox) odczytanego kształtu
+plt.xlim(shape_ex.bbox[0], shape_ex.bbox[2])
+plt.show()
+
+sf = shapefile.Reader('jezdnie.shp')
+shape_ex = sf.shape(3)  # Odczytaj czwarty kształt (indeks 3) - woj kuj pom
+
+record = records[3]  # Odczytaj czwarty rekord (indeks 3)
+
+x_lon = np.zeros((len(shape_ex.points), 1))
+y_lat = np.zeros((len(shape_ex.points), 1))
+
+for ip in range(len(shape_ex.points)):
+    x_lon[ip] = shape_ex.points[ip][0]
+    y_lat[ip] = shape_ex.points[ip][1]
+
+# Plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.get_yaxis().set_visible(True)
+ax.get_xaxis().set_visible(True)
+ax.set_frame_on(False)
+plt.plot(x_lon, y_lat, 'k')
+
+# Ustawiane są ograniczenia wykresu używając danych z bounding box (bbox) odczytanego kształtu
+plt.xlim(shape_ex.bbox[0], shape_ex.bbox[2])
+plt.show()
+
+sf = shapefile.Reader('cropPoints.dbf')
+shape_ex = sf.shape(3)  # Odczytaj czwarty kształt (indeks 3) - woj kuj pom
+
+record = records[3]  # Odczytaj czwarty rekord (indeks 3)
+
+x_lon = shape_ex.points[0]
+
+# Plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.get_yaxis().set_visible(True)
+ax.get_xaxis().set_visible(True)
+ax.set_frame_on(False)
+plt.plot(x_lon[0], x_lon[1], marker="o", markersize=20, markeredgecolor="red", markerfacecolor="green")
+
+# Ustawiane są ograniczenia wykresu używając danych z bounding box (bbox) odczytanego kształtu
+plt.show()
