04183efe5ce4e448ef6539e72befb22757d877bb
lisachu98 <52169344+lisachu98@users.noreply.github.com>
2023-05-29 10:30:04 +0200

    Added multipoint

--- records.py
+++ records.py
@@ -77,3 +77,27 @@ plt.plot(x_lon[0], x_lon[1], marker="o", markersize=20, markeredgecolor="red", m
 
 # Ustawiane są ograniczenia wykresu używając danych z bounding box (bbox) odczytanego kształtu
 plt.show()
+
+sf = shapefile.Reader('upenn-tc_br18s22_2r.shp')
+shape_ex = sf.shape(0)  # Odczytaj czwarty kształt (indeks 3) - woj kuj pom
+
+record = records[0]  # Odczytaj czwarty rekord (indeks 3)
+
+x_lon = np.zeros((len(shape_ex.points), 1))
+y_lat = np.zeros((len(shape_ex.points), 1))
+
+for ip in range(len(shape_ex.points)):
+    x_lon[ip] = shape_ex.points[ip][0]
+    y_lat[ip] = shape_ex.points[ip][1]
+
+# Plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.get_yaxis().set_visible(True)
+ax.get_xaxis().set_visible(True)
+ax.set_frame_on(False)
+plt.plot(x_lon, y_lat, 'o')
+
+# Ustawiane są ograniczenia wykresu używając danych z bounding box (bbox) odczytanego kształtu
+plt.xlim(shape_ex.bbox[0]-5, shape_ex.bbox[2]+5)
+plt.show()
